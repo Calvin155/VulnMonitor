3045b94338e67142d1fc83f731339bef394fc9a9
Calvin155 <calvinlynch181@outlook.com>
2026-05-03 22:08:31 +0100

    server update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // 🔥 THIS IS THE FIX
+    port: 5173, // optional but nice to fix port
     proxy: {
       '/api': 'http://localhost:3001',
       '/pentester': {
